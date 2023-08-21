768341815e9f5b5fbda9d630e54b5227f4e2a4b7
Sntree2mi8 <sntree238@gmail.com>
2023-08-21 15:06:33 +0900

    refactor

--- parser/misc.go
+++ parser/misc.go
@@ -6,38 +6,6 @@ import (
 	"github.com/Sntree2mi8/gogqlparser/ast"
 )
 
-func ParseArgumentsDefinition(l *LexerWrapper) (defs []ast.InputValueDefinition, err error) {
-	t := l.NextToken()
-	if t.Kind != gogqllexer.ParenL {
-		return defs, err
-	}
-
-	for {
-		inputValDescription := ""
-
-		t = l.PeekToken()
-		switch t.Kind {
-		case gogqllexer.String, gogqllexer.BlockString:
-			inputValDescription = t.Value
-			l.NextToken()
-		case gogqllexer.Name:
-			ivd, err := parseInputValueDefinition(l, inputValDescription)
-			if err != nil {
-				return defs, err
-			}
-			defs = append(defs, ivd)
-		case gogqllexer.ParenR:
-			if len(defs) == 0 {
-				return defs, fmt.Errorf("unexpected token %+v", t)
-			}
-			l.NextToken()
-			return defs, nil
-		default:
-			return defs, fmt.Errorf("unexpected token %+v", t)
-		}
-	}
-}
-
 func parseType(l *LexerWrapper) (t ast.Type, err error) {
 	var token gogqllexer.Token
 
