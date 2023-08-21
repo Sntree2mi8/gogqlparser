acac04b371f613b225dfda80462c9cda236d9d1f
Sntree2mi8 <sntree238@gmail.com>
2023-08-21 14:42:11 +0900

    move process

--- parser/directive.go
+++ parser/directive.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"github.com/Sntree2mi8/gogqllexer"
 	"github.com/Sntree2mi8/gogqlparser/ast"
 )
@@ -75,4 +76,85 @@ func parseDirective(l *LexerWrapper) (d ast.Directive, err error) {
 	}
 
 	return d, err
+}
+
+func ParseDirectiveDefinition(l *LexerWrapper, description string) (def *ast.DirectiveDefinition, err error) {
+	l.NextToken()
+	t := l.NextToken()
+	if t.Kind != gogqllexer.At {
+		return nil, fmt.Errorf("unexpected token %+v", t)
+	}
+	t = l.NextToken()
+	if t.Kind != gogqllexer.Name {
+		return nil, fmt.Errorf("unexpected token %+v", t)
+	}
+
+	directiveName := t.Value
+	isRepeatable := false
+
+	t = l.PeekToken()
+	var inputValueDefinitions []ast.InputValueDefinition
+	if t.Kind == gogqllexer.ParenL {
+		inputValueDefinitions, err = ParseArgumentsDefinition(l)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	t = l.PeekToken()
+	if t.Kind != gogqllexer.Name {
+		return nil, fmt.Errorf("unexpected token %+v", t)
+	}
+
+	if t.Value == "repeatable" {
+		isRepeatable = true
+		l.NextToken()
+	}
+
+	t = l.NextToken()
+	if t.Kind != gogqllexer.Name {
+		return nil, fmt.Errorf("unexpected token %+v", t)
+	} else if t.Value != "on" {
+		return nil, fmt.Errorf("unexpected token %+v", t)
+	}
+
+	t = l.NextToken()
+	if t.Kind != gogqllexer.Name {
+		return nil, fmt.Errorf("unexpected token %+v", t)
+	}
+	dLocations := make([]ast.DirectiveLocation, 0)
+	dl := ParsDirectiveLocation(t.Value)
+	if dl == ast.DirectiveLocationUnknown {
+		return nil, fmt.Errorf("unexpected token %+v", t)
+	}
+	dLocations = append(dLocations, dl)
+
+	for {
+		t = l.PeekToken()
+		if t.Kind != gogqllexer.Pipe {
+			break
+		} else {
+			l.NextToken()
+		}
+
+		t = l.NextToken()
+		if t.Kind != gogqllexer.Name {
+			return nil, fmt.Errorf("unexpected token %+v", t)
+		}
+		dl := ParsDirectiveLocation(t.Value)
+		if dl == ast.DirectiveLocationUnknown {
+			return nil, fmt.Errorf("unexpected token %+v", t)
+		}
+		dLocations = append(dLocations, dl)
+	}
+
+	def = &ast.DirectiveDefinition{
+		Description:         description,
+		Name:                directiveName,
+		ArgumentsDefinition: inputValueDefinitions,
+		IsRepeatable:        isRepeatable,
+		DirectiveLocations:  dLocations,
+	}
+
+	return def, nil
 }
