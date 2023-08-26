a6ff62e83b365d9648059dcbd55f00fd12d40a72
Sntree2mi8 <sntree238@gmail.com>
2023-08-26 15:21:39 +0900

    directiveのvalidationを実装

--- parser/validation.go
+++ parser/validation.go
@@ -3,39 +3,152 @@ package parser
 import (
 	"fmt"
 	"github.com/Sntree2mi8/gogqlparser/ast"
+	"slices"
 	"strings"
 )
 
-// This is a place to put validation related functions. At the time of creation, I couldn't imagine how to divide it, so I'll put it here for now.
 func ValidateTypeSystemExtensionDocument(doc *ast.TypeSystemExtensionDocument) error {
-	return validateDirectiveDefinitions(doc)
+	return newValidator(doc).validateDirectiveDefinitions()
 }
 
-func validateDirectiveDefinitions(doc *ast.TypeSystemExtensionDocument) error {
-	// validate directive
-	// 1. A directive definition must not contain the use of a directive which references itself directly.
-	// TODO: implement
+type validator struct {
+	doc *ast.TypeSystemExtensionDocument
 
-	// 2. A directive definition must not contain the use of a directive which references itself indirectly by referencing a Type or Directive which transitively includes a reference to this directive.
-	// TODO: implement
+	typeDefs      map[string]ast.TypeDefinition
+	directiveDefs map[string]ast.DirectiveDefinition
+}
 
-	// 3. The directive must not have a name which begins with the characters "__" (two underscores).
-	for _, dd := range doc.DirectiveDefinitions {
+func newValidator(doc *ast.TypeSystemExtensionDocument) *validator {
+	typeDefs := make(map[string]ast.TypeDefinition, len(doc.TypeDefinitions))
+	for _, def := range doc.TypeDefinitions {
+		typeDefs[def.TypeName()] = def
+	}
+
+	directiveDefs := make(map[string]ast.DirectiveDefinition, len(doc.DirectiveDefinitions))
+	for _, def := range doc.DirectiveDefinitions {
+		directiveDefs[def.Name] = def
+	}
+
+	return &validator{
+		doc:           doc,
+		typeDefs:      typeDefs,
+		directiveDefs: directiveDefs,
+	}
+}
+
+func (v *validator) validateDirectiveDefinitions() error {
+	var checkReferenceInDirective func(dd ast.DirectiveDefinition, d ast.Directive) (hasReference bool)
+	checkReferenceInDirective = func(dd ast.DirectiveDefinition, d ast.Directive) (hasReference bool) {
+		if d.Name == dd.Name {
+			return true
+		}
+
+		for _, ad := range v.directiveDefs[d.Name].ArgumentsDefinition {
+			for _, adDir := range ad.Directives {
+				return checkReferenceInDirective(dd, adDir)
+			}
+		}
+
+		return false
+	}
+
+	var checkReferenceInType func(dd ast.DirectiveDefinition, t ast.Type) (hasReference bool)
+	checkReferenceInType = func(dd ast.DirectiveDefinition, t ast.Type) (hasReference bool) {
+		typeDef := v.typeDefs[t.NamedType]
+
+		switch typeDef.TypeDefinitionKind() {
+		case ast.TypeDefinitionKindScalar:
+			// TODO: 本当はscalarにもdirectiveをつけられるけど、まだ実装できていない
+			return false
+		case ast.TypeDefinitionKindEnum:
+			if slices.Contains(dd.DirectiveLocations, ast.DirectiveLocationEnum) {
+				for _, td := range typeDef.GetDirectives() {
+					if checkReferenceInDirective(dd, td) {
+						return true
+					}
+				}
+			}
+			if slices.Contains(dd.DirectiveLocations, ast.DirectiveLocationEnumValue) {
+				enumTypeDef := typeDef.(*ast.EnumTypeDefinition)
+				for _, ev := range enumTypeDef.EnumValue {
+					for _, evd := range ev.Directives {
+						if evd.Name == dd.Name {
+							return true
+						}
+
+						if checkReferenceInDirective(dd, evd) {
+							return true
+						}
+					}
+				}
+			}
+		case ast.TypeDefinitionKindInputObject:
+			if slices.Contains(dd.DirectiveLocations, ast.DirectiveLocationInputObject) {
+				for _, td := range typeDef.GetDirectives() {
+					if checkReferenceInDirective(dd, td) {
+						return true
+					}
+				}
+			}
+			if slices.Contains(dd.DirectiveLocations, ast.DirectiveLocationInputFieldDefinition) {
+				inputObjectTypeDef := typeDef.(*ast.InputObjectTypeDefinition)
+				for _, ifd := range inputObjectTypeDef.InputFields {
+					for _, ifdd := range ifd.Directives {
+						if checkReferenceInDirective(dd, ifdd) {
+							return true
+						}
+					}
+				}
+			}
+		default:
+			return false
+		}
+
+		return false
+	}
+
+	var isInputType func(t ast.Type) bool
+	isInputType = func(t ast.Type) bool {
+		if t.ListType != nil {
+			return isInputType(*t.ListType)
+		}
+
+		td := v.typeDefs[t.NamedType]
+		switch td.TypeDefinitionKind() {
+		case ast.TypeDefinitionKindScalar, ast.TypeDefinitionKindEnum, ast.TypeDefinitionKindInputObject:
+			return true
+		default:
+			return false
+		}
+	}
+
+	for _, dd := range v.doc.DirectiveDefinitions {
 		if strings.HasPrefix(dd.Name, "__") {
 			return fmt.Errorf("directive name must not begins with \"__\": %s", dd.Name)
 		}
 
-		// for each argument
+		canUseOnArgumentDefinition := slices.Contains(dd.DirectiveLocations, ast.DirectiveLocationArgumentDefinition)
 		for _, ad := range dd.ArgumentsDefinition {
-			// 4. The argument must not have a name which begins with the characters "__" (two underscores).
+			if canUseOnArgumentDefinition {
+				for _, adDir := range ad.Directives {
+					// argumentDefinitionに使用したdirectiveが直接参照もしくは間接的に参照をしていないかを確認する
+					if checkReferenceInDirective(dd, adDir) {
+						return fmt.Errorf("directive %s must not contain the use of a directive which references itself", dd.Name)
+					}
+				}
+			}
+
 			if strings.HasPrefix(ad.Name, "__") {
 				return fmt.Errorf("argument name must not begins with \"__\": %s", ad.Name)
 			}
 
-			// 5. The argument must accept a type where IsInputType(argumentType) returns true.
-			// これを判断するためにはtype definitionsが必要になる
-			// argumentに割り当てられた方をtype definitionsから探してそれがinputTypeである必要がある
-			// TODO: implement
+			if !isInputType(ad.Type) {
+				return fmt.Errorf("argument %s must accept a type where IsInputType(argumentType) returns true", ad.Name)
+			}
+
+			if checkReferenceInType(dd, ad.Type) {
+				return fmt.Errorf("argument %s must not contain the use of a directive which references itself", ad.Name)
+			}
 		}
 	}
 
--- parser/validation_test.go
+++ parser/validation_test.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"github.com/Sntree2mi8/gogqlparser/ast"
+	"log"
 	"testing"
 )
 
@@ -54,11 +55,206 @@ func Test_validateDirectiveDefinitions(t *testing.T) {
 			},
 			wantErr: true,
 		},
+		{
+			name: "The directive must not contain the use of a directive which references itself directly",
+			args: args{
+				doc: &ast.TypeSystemExtensionDocument{
+					DirectiveDefinitions: []ast.DirectiveDefinition{
+						{
+							Name: "test",
+							ArgumentsDefinition: []ast.InputValueDefinition{
+								{
+									Name: "__arg",
+									Type: ast.Type{
+										NamedType: "String",
+									},
+									Directives: []ast.Directive{
+										{
+											Name: "test",
+										},
+									},
+								},
+							},
+							DirectiveLocations: []ast.DirectiveLocation{
+								ast.DirectiveLocationInputFieldDefinition,
+							},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "A directive definition must not contain the use of a directive which references itself indirectly by referencing a Type or Directive which transitively includes a reference to this directive.",
+			args: args{
+				doc: &ast.TypeSystemExtensionDocument{
+					TypeDefinitions: []ast.TypeDefinition{
+						&ast.InputObjectTypeDefinition{
+							Name: "testInputObject",
+							Directives: []ast.Directive{
+								{
+									Name: "test",
+								},
+							},
+						},
+					},
+					DirectiveDefinitions: []ast.DirectiveDefinition{
+						{
+							Name: "test",
+							ArgumentsDefinition: []ast.InputValueDefinition{
+								{
+									Name: "arg",
+									Type: ast.Type{
+										NamedType: "testInputObject",
+									},
+								},
+							},
+							DirectiveLocations: []ast.DirectiveLocation{
+								ast.DirectiveLocationInputObject,
+							},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "A directive definition must not contain the use of a directive which references itself indirectly by referencing a Type or Directive which transitively includes a reference to this directive.",
+			args: args{
+				doc: &ast.TypeSystemExtensionDocument{
+					TypeDefinitions: []ast.TypeDefinition{
+						&ast.InputObjectTypeDefinition{
+							Name: "testInputObject",
+							InputFields: []ast.InputValueDefinition{
+								{
+									Name: "test",
+									Type: ast.Type{
+										NamedType: "String",
+									},
+									Directives: []ast.Directive{
+										{
+											Name: "directiveForInputFieldDefinition",
+										},
+									},
+								},
+							},
+						},
+					},
+					DirectiveDefinitions: []ast.DirectiveDefinition{
+						{
+							Name: "directiveForInputFieldDefinition",
+							ArgumentsDefinition: []ast.InputValueDefinition{
+								{
+									Name: "arg",
+									Type: ast.Type{
+										NamedType: "testInputObject",
+									},
+								},
+							},
+							DirectiveLocations: []ast.DirectiveLocation{
+								ast.DirectiveLocationInputFieldDefinition,
+							},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Argument type must be scalar, enum, input object",
+			args: args{
+				doc: &ast.TypeSystemExtensionDocument{
+					TypeDefinitions: []ast.TypeDefinition{
+						&ast.ObjectTypeDefinition{
+							Name: "TestObject",
+						},
+					},
+					DirectiveDefinitions: []ast.DirectiveDefinition{
+						{
+							Name: "test",
+							ArgumentsDefinition: []ast.InputValueDefinition{
+								{
+									Name: "__arg",
+									Type: ast.Type{
+										NamedType: "TestObject",
+									},
+								},
+							},
+							DirectiveLocations: []ast.DirectiveLocation{
+								ast.DirectiveLocationInputFieldDefinition,
+							},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Argument type must be scalar, enum, input object",
+			args: args{
+				doc: &ast.TypeSystemExtensionDocument{
+					TypeDefinitions: []ast.TypeDefinition{
+						&ast.UnionTypeDefinition{
+							Name: "TestUnion",
+						},
+					},
+					DirectiveDefinitions: []ast.DirectiveDefinition{
+						{
+							Name: "test",
+							ArgumentsDefinition: []ast.InputValueDefinition{
+								{
+									Name: "__arg",
+									Type: ast.Type{
+										NamedType: "TestUnion",
+									},
+								},
+							},
+							DirectiveLocations: []ast.DirectiveLocation{
+								ast.DirectiveLocationInputFieldDefinition,
+							},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Argument type must be scalar, enum, input object",
+			args: args{
+				doc: &ast.TypeSystemExtensionDocument{
+					TypeDefinitions: []ast.TypeDefinition{
+						&ast.InterfaceTypeDefinition{
+							Name: "TestInterface",
+						},
+					},
+					DirectiveDefinitions: []ast.DirectiveDefinition{
+						{
+							Name: "test",
+							ArgumentsDefinition: []ast.InputValueDefinition{
+								{
+									Name: "__arg",
+									Type: ast.Type{
+										NamedType: "TestInterface",
+									},
+								},
+							},
+							DirectiveLocations: []ast.DirectiveLocation{
+								ast.DirectiveLocationInputFieldDefinition,
+							},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := validateDirectiveDefinitions(tt.args.doc); (err != nil) != tt.wantErr {
+			v := newValidator(tt.args.doc)
+			if err := v.validateDirectiveDefinitions(); (err != nil) != tt.wantErr {
 				t.Errorf("validateDirectiveDefinitions() error = %v, wantErr %v", err, tt.wantErr)
+			} else {
+				log.Println(err)
 			}
 		})
 	}
