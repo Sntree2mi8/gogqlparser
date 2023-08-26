068b8d3de8910b64fb93acf1c5a05ac7363b745a
Sntree2mi8 <sntree238@gmail.com>
2023-08-26 17:16:35 +0900

    関数を外だししてわかりやすくした

--- validator/directive.go
+++ validator/directive.go
@@ -8,96 +8,93 @@ import (
 	"strings"
 )
 
+func (v *validator) isInputType(t ast.Type) bool {
+	if t.ListType != nil {
+		return v.isInputType(*t.ListType)
+	}
+
+	td, ok := v.typeDefs[t.NamedType]
+	if !ok {
+		log.Println("type not found: ", t.NamedType)
+		return false
+	}
+	switch td.TypeDefinitionKind() {
+	case ast.TypeDefinitionKindScalar, ast.TypeDefinitionKindEnum, ast.TypeDefinitionKindInputObject:
+		return true
+	default:
+		return false
+	}
+}
+
+func (v *validator) checkDirectiveReferenceInDirective(dd ast.DirectiveDefinition, d ast.Directive) (hasReference bool) {
+	if d.Name == dd.Name {
+		return true
+	}
+
+	for _, ad := range v.directiveDefs[d.Name].ArgumentsDefinition {
+		for _, adDir := range ad.Directives {
+			return v.checkDirectiveReferenceInDirective(dd, adDir)
+		}
+	}
+
+	return false
+}
+
+func (v *validator) checkDirectiveReferenceInType(dd ast.DirectiveDefinition, t ast.Type) (hasReference bool) {
+	typeDef := v.typeDefs[t.NamedType]
+
+	switch typeDef.TypeDefinitionKind() {
+	case ast.TypeDefinitionKindScalar:
+		// TODO: 本当はscalarにもdirectiveをつけられるけど、まだ実装できていない
+		return false
+	case ast.TypeDefinitionKindEnum:
+		if slices.Contains(dd.DirectiveLocations, ast.DirectiveLocationEnum) {
+			for _, td := range typeDef.GetDirectives() {
+				if v.checkDirectiveReferenceInDirective(dd, td) {
+					return true
+				}
+			}
+		}
+		if slices.Contains(dd.DirectiveLocations, ast.DirectiveLocationEnumValue) {
+			enumTypeDef := typeDef.(*ast.EnumTypeDefinition)
+			for _, ev := range enumTypeDef.EnumValue {
+				for _, evd := range ev.Directives {
+					if evd.Name == dd.Name {
+						return true
+					}
+
+					if v.checkDirectiveReferenceInDirective(dd, evd) {
+						return true
+					}
+				}
+			}
+		}
+	case ast.TypeDefinitionKindInputObject:
+		if slices.Contains(dd.DirectiveLocations, ast.DirectiveLocationInputObject) {
+			for _, td := range typeDef.GetDirectives() {
+				if v.checkDirectiveReferenceInDirective(dd, td) {
+					return true
+				}
+			}
+		}
+		if slices.Contains(dd.DirectiveLocations, ast.DirectiveLocationInputFieldDefinition) {
+			inputObjectTypeDef := typeDef.(*ast.InputObjectTypeDefinition)
+			for _, ifd := range inputObjectTypeDef.InputFields {
+				for _, ifdd := range ifd.Directives {
+					if v.checkDirectiveReferenceInDirective(dd, ifdd) {
+						return true
+					}
+				}
+			}
+		}
+	default:
+		return false
+	}
+
+	return false
+}
+
 func (v *validator) validateDirectiveDefinitions() error {
-	var checkReferenceInDirective func(dd ast.DirectiveDefinition, d ast.Directive) (hasReference bool)
-	checkReferenceInDirective = func(dd ast.DirectiveDefinition, d ast.Directive) (hasReference bool) {
-		if d.Name == dd.Name {
-			return true
-		}
-
-		for _, ad := range v.directiveDefs[d.Name].ArgumentsDefinition {
-			for _, adDir := range ad.Directives {
-				return checkReferenceInDirective(dd, adDir)
-			}
-		}
-
-		return false
-	}
-
-	var checkReferenceInType func(dd ast.DirectiveDefinition, t ast.Type) (hasReference bool)
-	checkReferenceInType = func(dd ast.DirectiveDefinition, t ast.Type) (hasReference bool) {
-		typeDef := v.typeDefs[t.NamedType]
-
-		switch typeDef.TypeDefinitionKind() {
-		case ast.TypeDefinitionKindScalar:
-			// TODO: 本当はscalarにもdirectiveをつけられるけど、まだ実装できていない
-			return false
-		case ast.TypeDefinitionKindEnum:
-			if slices.Contains(dd.DirectiveLocations, ast.DirectiveLocationEnum) {
-				for _, td := range typeDef.GetDirectives() {
-					if checkReferenceInDirective(dd, td) {
-						return true
-					}
-				}
-			}
-			if slices.Contains(dd.DirectiveLocations, ast.DirectiveLocationEnumValue) {
-				enumTypeDef := typeDef.(*ast.EnumTypeDefinition)
-				for _, ev := range enumTypeDef.EnumValue {
-					for _, evd := range ev.Directives {
-						if evd.Name == dd.Name {
-							return true
-						}
-
-						if checkReferenceInDirective(dd, evd) {
-							return true
-						}
-					}
-				}
-			}
-		case ast.TypeDefinitionKindInputObject:
-			if slices.Contains(dd.DirectiveLocations, ast.DirectiveLocationInputObject) {
-				for _, td := range typeDef.GetDirectives() {
-					if checkReferenceInDirective(dd, td) {
-						return true
-					}
-				}
-			}
-			if slices.Contains(dd.DirectiveLocations, ast.DirectiveLocationInputFieldDefinition) {
-				inputObjectTypeDef := typeDef.(*ast.InputObjectTypeDefinition)
-				for _, ifd := range inputObjectTypeDef.InputFields {
-					for _, ifdd := range ifd.Directives {
-						if checkReferenceInDirective(dd, ifdd) {
-							return true
-						}
-					}
-				}
-			}
-		default:
-			return false
-		}
-
-		return false
-	}
-
-	var isInputType func(t ast.Type) bool
-	isInputType = func(t ast.Type) bool {
-		if t.ListType != nil {
-			return isInputType(*t.ListType)
-		}
-
-		td, ok := v.typeDefs[t.NamedType]
-		if !ok {
-			log.Println("type not found: ", t.NamedType)
-			return false
-		}
-		switch td.TypeDefinitionKind() {
-		case ast.TypeDefinitionKindScalar, ast.TypeDefinitionKindEnum, ast.TypeDefinitionKindInputObject:
-			return true
-		default:
-			return false
-		}
-	}
-
 	for _, dd := range v.doc.DirectiveDefinitions {
 		if strings.HasPrefix(dd.Name, "__") {
 			return fmt.Errorf("directive name must not begins with \"__\": %s", dd.Name)
@@ -108,7 +105,7 @@ func (v *validator) validateDirectiveDefinitions() error {
 			if canUseOnArgumentDefinition {
 				for _, adDir := range ad.Directives {
 					// argumentDefinitionに使用したdirectiveが直接参照もしくは間接的に参照をしていないかを確認する
-					if checkReferenceInDirective(dd, adDir) {
+					if v.checkDirectiveReferenceInDirective(dd, adDir) {
 						return fmt.Errorf("directive %s must not contain the use of a directive which references itself", dd.Name)
 					}
 				}
@@ -118,11 +115,11 @@ func (v *validator) validateDirectiveDefinitions() error {
 				return fmt.Errorf("argument name must not begins with \"__\": %s", ad.Name)
 			}
 
-			if !isInputType(ad.Type) {
+			if !v.isInputType(ad.Type) {
 				return fmt.Errorf("argument %s must accept a type where IsInputType(argumentType) returns true", ad.Name)
 			}
 
-			if checkReferenceInType(dd, ad.Type) {
+			if v.checkDirectiveReferenceInType(dd, ad.Type) {
 				return fmt.Errorf("argument %s must not contain the use of a directive which references itself", ad.Name)
 			}
 		}
